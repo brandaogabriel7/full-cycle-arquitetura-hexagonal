4bc77382c8a6e649781d930f5ec385883849619b
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2024-06-12 20:37:05 -0300

    test: database setup

---
+++ adapters/db/product.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/brandaogabriel7/full-cycle-arquitetura-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
---
+++ adapters/db/product_test.go
@@ -0,0 +1,37 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		"id" string,
+		"name" string,
+		"price" float,
+		"status" string
+	);`
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products values("abc", "Product Test", 0, "disabled");`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
