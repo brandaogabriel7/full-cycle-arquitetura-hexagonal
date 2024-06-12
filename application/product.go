08512dcf36ca683da6161d8954449d3e542e519f
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2024-06-12 19:52:10 -0300

    feat: product constructor

--- application/product.go
+++ application/product.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
 
 func init() {
@@ -30,6 +31,13 @@ type Product struct {
 	Name   string  `valid:"required"`
 	Price  float64 `valid:"float,optional"`
 	Status string  `valid:"required"`
+}
+
+func NewProduct() *Product {
+	return &Product{
+		ID:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
 }
 
 func (p *Product) IsValid() (bool, error) {
